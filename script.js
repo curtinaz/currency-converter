66476fc05c4ba7963130726d2dcd9df8c37dc0a9
Cristian Curtinaz <63929112+curtinaz@users.noreply.github.com>
2021-01-12 16:45:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,4 +7,9 @@ xhr.onload = function(){
   console.log(this.response);
   objeto = JSON.stringify(this.response);
   objeto = JSON.parse(objeto)
+  objeto = objeto.split(":")
+  objeto = JSON.stringify(objeto)
+  objeto = objeto.split(",")
+  objeto = JSON.parse(objeto)
+
 }
