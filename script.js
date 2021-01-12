bd82c6a79f3cff653b62055904c5fc37c97f87bc
Cristian Curtinaz <63929112+curtinaz@users.noreply.github.com>
2021-01-12 13:13:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 var xhr = new XMLHttpRequest();
 xhr.open("GET", "https://api.exchangeratesapi.io/latest", true);
 xhr.send();
-xhr.responseType = "text";
 xhr.onload = function(){
   console.log(this.response);
 }
